84e1f87f9eb28777ce7c512d1e73d98ab3ec111a
aidoitdev <aidoit.dev@gmail.com>
2025-06-21 22:54:43 +0530

    express

---
+++ ExpressJS/newProject/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/page", (request, response) => {
+  response.sendFile("./page.html", { root: __dirname });
+});
+
+app.listen(8000);
---
+++ ExpressJS/project/app.js
@@ -0,0 +1,82 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// MongoDB URL
+const mongoDBURL = 'mongodb://127.0.0.1:27017/studentDB';
+
+app.use(express.json())
+
+// Connect to MongoDB
+mongoose.connect(mongoDBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("✅ MongoDB connected"))
+    .catch((err) => console.error("❌ Connection Error:", err));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Connected to MongoDB!');
+});
+
+
+
+// Define Mongoose Schema & Model
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: String,
+    email: String
+}, {collection: 'validatedStudents'});
+
+const Student = mongoose.model('validatedStudents', studentSchema);
+
+// 👉 Endpoint to fetch all students
+app.get('/students', async (req, res) => {
+    try {
+        const students = await Student.find(); // Get all students
+        res.status(200).json(students);        // Respond with student data
+    } catch (err) {
+        res.status(500).json({ message: "Server error while fetching students." });
+    }
+});
+
+app.post("/poststudent", async(req, res) => {
+  try {
+
+
+    console.log(req.body, "the req")
+    const {name, age, email} = req.body;
+    if(!name || !age || !email){
+       return res.status(400).json({message: "Please fill all fields"});
+    }
+
+    const newStudent = new Student({name, age, email});
+
+    await newStudent.save()
+
+    res.status(201).json({message: "Student created successfully", newStudent});
+
+  }
+  catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Server error while fetching students.", error });
+  }
+})
+
+// app.get('/students/age22plus', async (req, res) => {
+//     try {
+//         const students = await Student.find({ age: { $gte: 22 } });
+//         res.status(200).json(students);
+//     } catch (err) {
+//         res.status(500).json({ message: "Error fetching students with age ≥ 20" });
+//     }
+// });
+
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
